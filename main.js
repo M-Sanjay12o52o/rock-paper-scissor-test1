0d40c940bf03c02f6e3c49f7f68f06f79b0980ea
M Sanjay <msnjy12o52o@gmail.com>
2023-06-22 11:45:42 +0530

    function playRound partially written

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-function getCompuerChoice() {
-  let ans = '';
-  let num = 0;
-  num = Math.floor((Math.random() * 3));
-  if (num == 0) {
-    ans = "rock";
-  } else if (num == 1) {
-    ans = "paper"; 
-  } else if (num == 2) {
-    ans = "scissors";
+// function getCompuerChoice() {
+//   let ans = '';
+//   let num = 0;
+//   num = Math.floor((Math.random() * 3));
+//   if (num == 0) {
+//     ans = "rock";
+//   } else if (num == 1) {
+//     ans = "paper"; 
+//   } else if (num == 2) {
+//     ans = "scissors";
+//   }
+//   return ans;
+// }
+
+// console.log(getCompuerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  let ps = playerSelection.toLowerCase();
+  let cs = computerSelection.toLowerCase();
+
+  if (ps === cs) {
+    return "It's a Tie!";
+  } else if (ps == 'rock' && cs == 'paper') {
+    return "You Lose! Paper beats Rock"; 
+  } else if (ps == 'paper' && cs == 'scissors') {
+    return "You Lose! Scissors beats Paper"
   }
-  return ans;
 }
 
-console.log(getCompuerChoice());
+console.log(playRound('ROCK', 'PAPER'));
