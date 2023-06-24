caa08bf1a78a3cd7d1a893ea9870078cb2310e1e
M Sanjay <msnjy12o52o@gmail.com>
2023-06-24 08:06:52 +0530

    added scores tracker and finals scores and final result displayer

--- main.js
+++ main.js
@@ -15,9 +15,12 @@ function getCompuerChoice() {
 }
 
 let computerSelection = getCompuerChoice(); 
-console.log(computerSelection);
+// console.log(computerSelection);
 
 // Playround  Function
+
+let psScore = 0;
+let csScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   let ps = playerSelection.toLowerCase();
@@ -26,19 +29,54 @@ function playRound(playerSelection, computerSelection) {
   if (ps === cs) {
     return "It's a Tie!";
   } else if (ps == 'rock' && cs == 'paper') {
+    csScore += 1;
     return "You Lose! Paper beats Rock"; 
   } else if (ps == 'rock' && cs == 'scissors') {
+    csScore += 1;
     return "You Lose! Scissors beats Paper"
   } else if (ps == 'paper' && cs == 'rock') {
+    psScore += 1;
     return "You Win. Paper Covers Rock"
   } else if (ps == 'paper' && cs == 'scissors') {
+    csScore += 1;
     return "You Lose! Scissors cuts Paper"
   } else if (ps == 'scissors' && cs == 'paper') {
+    psScore += 1;
     return "You Win. Scissors cuts Paper"
   } else if (ps == 'scissors' && cs == 'rock') {
+    csScore += 1;
     return "You Lose! Rock beats Scissors"
+  }
+
+  return csScore, psScore;
+}
+
+console.log(playRound('rock', computerSelection));
+console.log(playRound('paper', computerSelection));
+console.log(playRound('scissors', computerSelection));
+console.log(playRound('scissors', computerSelection));
+console.log(playRound('paper', computerSelection));
+
+console.log("Computer's Score: ", csScore);
+console.log("Your Score: ", psScore);
+
+//Final Result
+function finalResult() {
+  if (csScore > psScore) {
+    console.log("You Lose. " + "Final Scores: " + "Computers - " + csScore + ". Your's - " + psScore);
+  } else if(psScore > csScore) {
+    console.log("You WIN.b" + "Final Scores: " + "Computers - " + csScore + ". Your's - " + psScore);
+  } else {
+    console.log("It's a TIE. " + "Final Scores: " + "Computers - " + csScore + ". Your's - " + psScore);
   }
 }
 
-console.log(playRound('ROCK', computerSelection));
+console.log(finalResult());
 
+// console.log(playRound('ROCK', computerSelection));
+
+// Function GAME
+
+
+
+// console.log(game());
